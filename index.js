8ed5d7bcbc973a3807912bb3497b374de0b1530b
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-08-08 11:45:32 -0400

    feat: add getKeys caching

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs").promises;
 const path = require("path");
 const Datalayer = require("chia-datalayer-wrapper");
 const { getChiaRoot } = require("chia-root-resolver");
+const defaultConfig = require("./defaultConfig");
 
 let config = defaultConfig;
 
@@ -16,10 +17,13 @@ const getCacheDirectory = () => {
   return `${chiaRoot}/data_layer/cache`;
 };
 
-const getValue = async (storeId, key) => {
+const getValue = async ({ id: storeId, key }) => {
   // Try to get the value from cache
   const cacheKey = `${storeId}-${key}`;
   let value = memoryCache.get(cacheKey);
+  if (value) {
+    console.log(`Served ${storeId} from memory cache`);
+  }
 
   if (value === undefined) {
     // If not in cache, try to get the value from the file system
@@ -27,24 +31,72 @@ const getValue = async (storeId, key) => {
     const filePath = path.join(cacheDirectory, storeId, key);
 
     try {
-      value = await fs.readFile(filePath, "utf8");
+      value = JSON.parse(await fs.readFile(filePath, "utf8"));
+      console.log(`Served ${storeId} from file cache`);
       memoryCache.set(cacheKey, value); // Cache the file content
     } catch (error) {
       if (error.code === "ENOENT") {
         // If the file does not exist, get the data from the datalayer
         const datalayer = Datalayer.rpc(config);
 
-        value = await datalayer.getValue(storeId, key); // Assuming the datalayer has a getValue method
+        value = await datalayer.getValue({ id: storeId, key });
 
         memoryCache.set(cacheKey, value);
 
         fs.mkdir(path.dirname(filePath), { recursive: true })
-          .then(() => fs.writeFile(filePath, JSON.stringify(data), "utf8"))
+          .then(() => fs.writeFile(filePath, JSON.stringify(value), "utf8"))
           .catch((err) => {
             console.error(err);
           });
       } else {
-        throw error; // Rethrow the error if it's not 'ENOENT'
+        return {
+          success: false,
+          error: error,
+        };
+      }
+    }
+  }
+
+  return value;
+};
+
+const getKeys = async ({ id: storeId }) => {
+  // Try to get the value from cache
+  const cacheKey = storeId;
+  let value = memoryCache.get(cacheKey);
+
+  if (value) {
+    console.log(`Served ${storeId} from memory cache`);
+  }
+
+  if (value === undefined) {
+    // If not in cache, try to get the value from the file system
+    const cacheDirectory = getCacheDirectory();
+    const filePath = path.join(cacheDirectory, storeId, storeId);
+
+    try {
+      value = JSON.parse(await fs.readFile(filePath, "utf8"));
+      console.log(`Served ${storeId} from file cache`);
+      memoryCache.set(cacheKey, value); // Cache the file content
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        // If the file does not exist, get the data from the datalayer
+        const datalayer = Datalayer.rpc(config);
+
+        value = await datalayer.getKeys({ id: storeId });
+
+        memoryCache.set(cacheKey, value);
+
+        fs.mkdir(path.dirname(filePath), { recursive: true })
+          .then(() => fs.writeFile(filePath, JSON.stringify(value), "utf8"))
+          .catch((err) => {
+            console.error(err);
+          });
+      } else {
+        return {
+          success: false,
+          error: error,
+        };
       }
     }
   }
@@ -54,19 +106,30 @@ const getValue = async (storeId, key) => {
 
 const invalidateCache = (storeId, key) => {
   // invalidate node-cache
-  const cacheKey = `${storeId}-${key}`;
-  memoryCache.del(cacheKey);
+  let cacheKey;
+  let filePath;
 
-  // delete the file if it exists
-  const cacheDirectory = getCacheDirectory();
-  const filePath = path.join(cacheDirectory, storeId, key);
+  if (key) {
+    cacheKey = `${storeId}-${key}`;
+    filePath = path.join(getCacheDirectory(), storeId, key);
+  } else {
+    cacheKey = storeId;
+    filePath = path.join(getCacheDirectory(), storeId);
+  }
 
+  memoryCache.keys().forEach((storedKey) => {
+    if (storedKey.startsWith(cacheKey)) {
+      memoryCache.del(storedKey);
+    }
+  });
+
+  // delete the file or directory if it exists
   fs.access(filePath, fs.constants.F_OK, (err) => {
     if (!err) {
-      // file exists, delete it
-      fs.unlink(filePath, (err) => {
+      // file or directory exists, delete it
+      fs.rm(filePath, { recursive: true, force: true }, (err) => {
         if (err) {
-          console.error(`Error while deleting the file: ${err}`);
+          console.error(`Error while deleting the file or directory: ${err}`);
         }
       });
     }
@@ -76,5 +139,6 @@ const invalidateCache = (storeId, key) => {
 module.exports = {
   configure,
   getValue,
+  getKeys,
   invalidateCache,
 };
